fdbf1496d20ced2888b87d882b2757e02da7c42d
Maliglood <maliglood@gmail.com>
2017-10-20 23:39:15 +0700

    Настройка Webpack'а.

--- src/library-reactjs/Front/main.js
+++ src/library-reactjs/Front/main.js
@@ -1,6 +1,6 @@
 "use strict";
 var React = require("react");
 var ReactDOM = require("react-dom");
-var Hello_1 = require("./components/Hello");
-ReactDOM.render(React.createElement(Hello_1.Hello, {compiler: "TypeScript", framework: "React"}), document.getElementById("example"));
+var hello_1 = require("./components/hello");
+ReactDOM.render(React.createElement(hello_1.Hello, {compiler: "TypeScript", framework: "React"}), document.getElementById("example"));
 //# sourceMappingURL=main.js.map
---
+++ src/library-reactjs/webpack.config.js
@@ -0,0 +1,50 @@
+﻿/// <binding BeforeBuild='Run - Development' />
+"use strict";
+
+var path = require('path');
+var CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        polyfills: "./Front/Scripts/polyfills.ts",
+        main: "./Front/main.tsx"
+    },
+
+    resolve: {
+        extensions: ['', '.ts', '.tsx', '.js', '.jsx', '.json', '.css']
+    },
+
+    output: {
+        // ReSharper disable once UndeclaredGlobalVariableUsing
+        path: path.join(__dirname, "./wwwroot/"),
+        filename: "[name].js"
+    },
+
+    devServer: {
+        contentBase: ".",
+        host: "localhost",
+        port: 9000
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.tsx?$/, exclude: /node_modules/, loader: "awesome-typescript-loader"
+            },
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
+};
