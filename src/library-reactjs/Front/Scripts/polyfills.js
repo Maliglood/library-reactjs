b8eee551ebc2063c8f9d86b66fe31739d910f329
Maliglood <maliglood@gmail.com>
2017-10-21 00:07:02 +0700

    Добавление js'ок в игнор Гита.

--- src/library-reactjs/Front/Scripts/polyfills.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-require('core-js/es6');
-require('reflect-metadata');
-require('zone.js/dist/zone');
-//# sourceMappingURL=polyfills.js.map
--- src/library-reactjs/Front/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var React = require("react");
-var ReactDOM = require("react-dom");
-var hello_1 = require("./components/hello");
-ReactDOM.render(React.createElement(hello_1.Hello, {compiler: "TypeScript", framework: "React"}), document.getElementById("example"));
-//# sourceMappingURL=main.js.map
